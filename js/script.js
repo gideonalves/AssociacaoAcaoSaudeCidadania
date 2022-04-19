949134861ef540fba91a8361df3f8e9b0f1a089d
gideonalves <gideonam20@gmail.com>
2022-04-19 10:50:40 -0300

    :bookmark: novo header

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
+//Menu 
 const btnMobile = document.getElementById('btn-mobile')
 
 function togglemenu(event) {
-    if (event.type === 'touchstart')event.preventDefault()
+    if (event.type === 'touchstart') event.preventDefault()
     // seleciona a tag #nav
     const nav = document.getElementById('nav')
     // adiciona uma class="active" dentro do nav ai vai ficar nav class="active"
@@ -9,12 +10,27 @@ function togglemenu(event) {
     const active = nav.classList.contains('active')
     event.currentTarget.setAttribute('aria-expanded', active)
     if (active) {
-    event.currentTarget.setAttribute('aria-label', 'Fechar Menu')
+        event.currentTarget.setAttribute('aria-label', 'Fechar Menu')
     } else {
-    event.currentTarget.setAttribute('aria-label', 'Abir Menu')
+        event.currentTarget.setAttribute('aria-label', 'Abir Menu')
     }
 }
 
 btnMobile.addEventListener("click", togglemenu)
 btnMobile.addEventListener("touchstart", togglemenu)/*para tira o delei na hora do click */
 
+
+
+
+// Ativar links do Menu
+const links = document.querySelectorAll('.header-menu a')
+
+function ativarLink(link) {
+    const url = location.href
+    const href = link.href
+    if (url.includes(href)) {
+        link.classList.add("ativo")
+    }
+}
+
+links.forEach(ativarLink)
